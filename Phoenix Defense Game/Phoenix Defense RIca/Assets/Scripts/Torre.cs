9283ba695538ac5fe2efff46e40b02e043a74e07
Bleysiker <henry3197@hotmail.com>
2020-03-18 12:58:44 -0500

    animaciones de magos y movimiento de personaje
    mas barra de vida ....

--- Phoenix Defense Game/Phoenix Defense RIca/Assets/Scripts/Torre.cs
+++ Phoenix Defense Game/Phoenix Defense RIca/Assets/Scripts/Torre.cs
@@ -7,12 +7,13 @@ public class Torre : MonoBehaviour {
     private GameObject enemigo;
     private bool esta_viva; 
     private float distancia_umbral=2;
+    private Animator controlador;
 
    
 
     void Start()
     {
-
+        controlador = this.GetComponent<Animator>();
     }
 	
     void Disparar()
@@ -20,6 +21,7 @@ public class Torre : MonoBehaviour {
         GameObject obj = (GameObject)Instantiate(GameObject.Find("Bala"), this.transform.position, Quaternion.identity);
         Bala bala = obj.GetComponent<Bala>();
         bala.ActivarBala(this);
+
     }
 	GameObject BuscarEnemigoCercano()
     {
@@ -41,7 +43,12 @@ public class Torre : MonoBehaviour {
         if (enemigo != null)
         {
             Disparar();
+            controlador.SetBool("atacar", true);
             Debug.DrawLine(this.transform.position, enemigo.transform.position, Color.yellow);
+        }
+        else
+        {
+            controlador.SetBool("atacar", false);
         }
 	}
     public GameObject Enemigo
